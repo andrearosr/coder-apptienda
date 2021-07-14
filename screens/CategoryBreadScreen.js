a86fe0618c3524358b41b16951a4a57cf6e4983e
Andrea Rosales <andrearosr@gmail.com>
2021-07-14 11:05:21 -0500

    Add category and bread store

--- screens/CategoryBreadScreen.js
+++ screens/CategoryBreadScreen.js
@@ -1,21 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { FlatList } from 'react-native';
-import { BREADS } from '../data/breads';
+import { useSelector, useDispatch } from 'react-redux';
 import BreadItem from '../components/BreadItem';
+import { filterBread, selectBread } from '../store/actions/bread.action';
 
-const CategoryBreadScreen = ({ navigation, route }) => {
-  const data = BREADS.filter(item => item.category === route.params.categoryID);
+const CategoryBreadScreen = ({ navigation }) => {
+  const dispatch = useDispatch();
+  const categoryBreads = useSelector(state => state.breads.filteredBreads);
+  const category = useSelector(state => state.categories.selected);
+
+  useEffect(() => {
+    dispatch(filterBread(category.id));
+  }, []);
+
   const handleSelected = (item) => {
-    navigation.navigate('DetailBread', {
-      item,
-      name: item.name,
-    });
+    dispatch(selectBread(item.id));
+    navigation.navigate('DetailBread', { name: item.name });
   }
   const renderItem = ({ item }) => <BreadItem item={item} onSelected={handleSelected} />
 
   return (
     <FlatList
-      data={data}
+      data={categoryBreads}
       renderItem={renderItem}
       keyExtractor={item => item.id}
     />
--- store/reducers/bread.reducer.js
+++ store/reducers/bread.reducer.js
@@ -1,13 +1,27 @@
 import { BREADS } from '../../data/breads';
+import { SELECT_BREAD, FILTER_BREAD } from '../actions/bread.action';
 
 const INITIAL_STATE = {
-  breads: BREADS,
+  list: BREADS,
   filteredBreads: [],
   selected: null,
 }
 
 const BreadReducer = (state = INITIAL_STATE, action) => {
-  return { ...state };
+  switch (action.type) {
+    case SELECT_BREAD:
+      return {
+        ...state,
+        selected: state.list.find(bread => bread.id === action.breadID),
+      };
+    case FILTER_BREAD:
+      return {
+        ...state,
+        filteredBreads: state.list.filter(bread => bread.category === action.categoryID),
+      }
+    default:
+      return { ...state };
+  }
 }
 
 export default BreadReducer;
