5931081237d6507c2f7af8e3bdcfe1ca463cf0ac
Andrea Rosales <andrearosr@gmail.com>
2021-07-13 20:27:25 -0500

    Setup store for categories screen

--- store/reducers/category.reducer.js
+++ store/reducers/category.reducer.js
@@ -1,12 +1,23 @@
 import { CATEGORIES } from '../../data/categories';
+import { SELECT_CATEGORY } from '../actions/category.action';
 
 const INITIAL_STATE = {
-  categories: CATEGORIES,
+  list: CATEGORIES,
   selected: null,
 }
 
 const CategoryReducer = (state = INITIAL_STATE, action) => {
-  return { ...state };
+  switch (action.type) {
+    case SELECT_CATEGORY:
+      const categoryIndex = state.list.findIndex(cat => cat.id === action.categoryID)
+      if (categoryIndex === -1) return { ...state };
+      return {
+        ...state,
+        selected: state.list[categoryIndex],
+      };
+    default:
+      return { ...state };
+  }
 }
 
 export default CategoryReducer;
