e408cfda95d68cfc9f6593ec1682139f56629f11
Andrea Rosales <andrearosr@gmail.com>
2021-09-08 20:47:32 -0500

    Add styles

--- screens/BreadDetailScreen.js
+++ screens/BreadDetailScreen.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
-import { FAB } from 'react-native-elements';
+import { FAB, Button } from 'react-native-elements';
 import { Ionicons } from '@expo/vector-icons';
 import { addItem } from '../store/actions/cart.actions';
 import { COLORS } from '../constants/colors';
@@ -18,11 +18,18 @@ export default function BreadDetailScreen() {
   
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>{bread.name}</Text>
-      <Text>{bread.description}</Text>
-      <Text>$ {bread.price}</Text>
-      <Text>{bread.weight}</Text>
-      <Button title="Agregar al carrito" onPress={handlerAddItemCart} />
+      <View style={styles.info}>
+        <Text style={styles.title}>{bread.name}</Text>
+        <Text>{bread.description}</Text>
+        <Text>$ {bread.price}</Text>
+        <Text>{bread.weight}</Text>
+      </View>
+      <Button
+        title="Agregar al carrito"
+        icon={<Ionicons name="add" size={24} color="white" />}
+        color={COLORS.primary}
+        onPress={handlerAddItemCart}
+      />
       <FAB
         icon={<Ionicons name="cart" size={24} color="white" />}
         placement="right"
@@ -39,6 +46,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  info: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 20,
   },
   title: {
     fontSize: 20,
